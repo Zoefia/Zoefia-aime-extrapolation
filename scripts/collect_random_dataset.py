5e52bfe1833183bb226c91248fbb77d824ba6f2a
Zoefia <116969138+Zoefia@users.noreply.github.com>
2024-02-13 20:09:40 +0000

    Update History

--- scripts/collect_random_dataset.py
+++ scripts/collect_random_dataset.py
@@ -6,4 +6,6 @@ from omegaconf import OmegaConf
 
 from aime.actor import RandomActor
 from aime.env import DMC, SaveTrajectories, TerminalSummaryWrapper
-from ai
+from aime.utils import CONFIG_PATH, DATA_PATH, interact_with_environment, setup_seed
+
+log.basicConfig(level=log.INF
