6c6a2f361bbd5e1f39323a6cd25dd6ade50baffa
Zoefia <116969138+Zoefia@users.noreply.github.com>
2024-02-17 12:09:53 +0000

    Updated binaries - automated checkin

--- scripts/collect_random_dataset.py
+++ scripts/collect_random_dataset.py
@@ -8,4 +8,10 @@ from aime.actor import RandomActor
 from aime.env import DMC, SaveTrajectories, TerminalSummaryWrapper
 from aime.utils import CONFIG_PATH, DATA_PATH, interact_with_environment, setup_seed
 
-log.basicConfig(level=log.INF
+log.basicConfig(level=log.INFO)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--env", type=str, required=True)
+    parser.add_argument("--num_tra
