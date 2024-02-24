84bc79fe1d5f3b22a8c867366cdeccafe329d138
Zoefia <116969138+Zoefia@users.noreply.github.com>
2024-02-24 17:13:00 +0000

    tg3: Fix race condition in tg3_get_stats64()

--- scripts/export_model_from_log.py
+++ scripts/export_model_from_log.py
@@ -1,1 +1,6 @@
-"This script helps to export the trained model 
+"This script helps to export the trained model from the log to the model folder"
+
+import os
+from argparse import ArgumentParser
+
+from aime.utils import MODEL
