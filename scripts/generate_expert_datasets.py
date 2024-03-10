58b80903e8e5bc3f001819a21901fc0c916f2a1f
Zoefia <116969138+Zoefia@users.noreply.github.com>
2024-03-10 09:47:27 +0000

    Version 2.1.0

--- scripts/generate_expert_datasets.py
+++ scripts/generate_expert_datasets.py
@@ -6,4 +6,6 @@ import torch
 from omegaconf import OmegaConf
 
 from aime.actor import PolicyActor
-from aime.env i
+from aime.env import DMC, SaveTrajectories, TerminalSummaryWrapper
+from aime.models.policy import TanhGaussianPolicy
+from ai
