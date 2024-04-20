cd97e4e308f8a633c7b6129622df9a028019c35e
Zoefia <116969138+Zoefia@users.noreply.github.com>
2024-04-20 06:25:45 +0000

    Header fix

--- scripts/mix_datasets.py
+++ scripts/mix_datasets.py
@@ -1,4 +1,8 @@
 import os
 from argparse import ArgumentParser
 
-from
+from aime.utils import DATA_PATH
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--input_da
