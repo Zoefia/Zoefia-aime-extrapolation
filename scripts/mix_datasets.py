665e28b6c251b23c3321a39110c8d55d415fa776
Zoefia <116969138+Zoefia@users.noreply.github.com>
2024-04-21 13:02:20 +0000

    suppression

--- scripts/mix_datasets.py
+++ scripts/mix_datasets.py
@@ -6,4 +6,8 @@ from aime.utils import DATA_PATH
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-i", "--input_dataset_names", type=str, nargs="+")
-    parser.add_argument("-o", "--output_dataset_name", type=str, required=T
+    parser.add_argument("-o", "--output_dataset_name", type=str, required=True)
+    args = parser.parse_args()
+
+    input_folders = [
+        os.path.join(DATA_PATH, dataset_name) for dataset_name in
